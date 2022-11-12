c6534b03b924ad7566f4af00f3e2dd16d800ed30
martin13s18 <117728421+MaRtin322s@users.noreply.github.com>
2022-11-12 19:08:48 +0200

    render navigation pages

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 
+router.get('/shared', (req, res) => {
+    res.render('trips/shared-trips');
+});
+
+router.get('/offer', (req, res) => {
+    res.render('trips/trip-create');
+});
+
+router.get('/profile', (req, res) => {
+    res.render('trips/profile');
+});
+
 module.exports = router;
