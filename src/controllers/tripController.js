91735b7d8da5a21fac82b4b249510ae8d253404f
martin13s18 <117728421+MaRtin322s@users.noreply.github.com>
2022-11-13 20:12:55 +0200

    Edit trip offer

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -37,7 +37,7 @@ router.get('/details/:tripId', async (req, res) => {
     const tripId = req.params.tripId;
     const trip = await tripService.getOne(tripId);
     const isOwner = trip.owner._id == req.user;
-    res.render('trips/trip-details', { 
+    res.render('trips/trip-details', {
         _id: trip._id,
         startPoint: trip.startPoint,
         endPoint: trip.endPoint,
@@ -50,13 +50,36 @@ router.get('/details/:tripId', async (req, res) => {
         description: trip.description,
         owner: trip.owner.email,
         creator: isOwner
-     });
+    });
 });
 
 router.get('/details/:tripId/delete', async (req, res) => {
     const tripId = req.params.tripId;
     await tripService.deleteTrip(tripId);
     res.redirect('/trips/shared');
+});
+
+router.get('/details/:tripId/edit', async (req, res) => {
+    const tripId = req.params.tripId;
+    const trip = await tripService.getOne(tripId);
+    res.render('trips/trip-edit', {
+        startPoint: trip.startPoint,
+        endPoint: trip.endPoint,
+        date: trip.date,
+        time: trip.time,
+        carImage: trip.carImage,
+        carBrand: trip.carBrand,
+        seats: trip.seats,
+        price: trip.price,
+        description: trip.description
+    });
+});
+
+router.post('/details/:tripId/edit', async (req, res) => {
+    const tripId = req.params.tripId;
+    const tripData = req.body;
+    await tripService.editTrip(tripId, tripData);
+    res.redirect(`/trips/details/${tripId}`);
 });
 
 module.exports = router;
--- src/services/TripService.js
+++ src/services/TripService.js
@@ -1,6 +1,7 @@
-const Trip =require('../models/Trip');
+const Trip = require('../models/Trip');
 
 exports.createTrip = async (data) => await Trip.create(data);
 exports.getAll = async () => await Trip.find().lean();
 exports.getOne = async (tripId) => await Trip.findById({ _id: tripId }).populate('owner');
 exports.deleteTrip = async (tripId) => await Trip.findByIdAndDelete({ _id: tripId });
+exports.editTrip = async (tripId, tripData) => await Trip.findByIdAndUpdate({ _id: tripId }, tripData );
