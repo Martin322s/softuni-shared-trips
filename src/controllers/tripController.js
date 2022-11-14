e5f31f91e5d7c221665953e0f14784bf28020e61
martin13s18 <117728421+MaRtin322s@users.noreply.github.com>
2022-11-14 19:25:42 +0200

    render profile and join trip

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -12,7 +12,7 @@ router.get('/offer', (req, res) => {
 });
 
 router.post('/offer', async (req, res) => {
-    const data = req.body;
+    const data = {startPoint, endPoint, date, time, carImage, carBrand, seats, price, description } = req.body;
     const owner = req.user;
 
     try {
@@ -21,7 +21,9 @@ router.post('/offer', async (req, res) => {
                 message: "All fields are required!"
             }
         } else {
-            await tripService.createTrip({ ...data, owner });
+            const trip = await tripService.createTrip({ seats: Number(seats) , owner,  ...data });
+            const author = await tripService.getAuthor(owner);
+            await tripService.addOffer(trip, author._id);
             res.redirect('/trips/shared');
         }
     } catch (err) {
@@ -29,14 +31,26 @@ router.post('/offer', async (req, res) => {
     }
 });
 
-router.get('/profile', (req, res) => {
-    res.render('trips/profile');
+router.get('/profile', async (req, res) => {
+    const user = await tripService.getAuthor(req.user);
+    const trips = await tripService.myOffers(req.user);
+    const myOffers = trips.map(x => { return { startPoint: x.startPoint, endPoint: x.endPoint, date: x.date, time: x.time } });
+    res.render('trips/profile', {
+        email: user.email,
+        count: user.tripsHistory.length,
+        haveTrips: user.tripsHistory.length > 0,
+        isMale: user.gender == "male",
+        trips: myOffers
+    });
 });
 
 router.get('/details/:tripId', async (req, res) => {
     const tripId = req.params.tripId;
     const trip = await tripService.getOne(tripId);
     const isOwner = trip.owner._id == req.user;
+    const mapped = trip.buddies.map(x => x.toString());
+    const isJoined = mapped.includes(req.user);
+    const buddiesCount = trip.buddies.length;
     res.render('trips/trip-details', {
         _id: trip._id,
         startPoint: trip.startPoint,
@@ -49,7 +63,10 @@ router.get('/details/:tripId', async (req, res) => {
         price: trip.price,
         description: trip.description,
         owner: trip.owner.email,
-        creator: isOwner
+        creator: isOwner,
+        aviableSeats: trip.seats > 0,
+        isJoined: isJoined,
+        buddiesCount: buddiesCount > 0
     });
 });
 
@@ -79,6 +96,16 @@ router.post('/details/:tripId/edit', async (req, res) => {
     const tripId = req.params.tripId;
     const tripData = req.body;
     await tripService.editTrip(tripId, tripData);
+    res.redirect(`/trips/details/${tripId}`);
+});
+
+router.get('/details/:tripId/join', async (req, res) => {
+    const tripId = req.params.tripId;
+    const user = req.user;
+    const trip = await tripService.getOne(tripId);
+    await tripService.joinTrip(tripId, user);
+    const seats = Number(trip.seats) - 1;
+    await tripService.editSeats(tripId, seats);
     res.redirect(`/trips/details/${tripId}`);
 });
 
--- src/services/TripService.js
+++ src/services/TripService.js
@@ -1,7 +1,13 @@
 const Trip = require('../models/Trip');
+const User = require('../models/User');
 
 exports.createTrip = async (data) => await Trip.create(data);
 exports.getAll = async () => await Trip.find().lean();
 exports.getOne = async (tripId) => await Trip.findById({ _id: tripId }).populate('owner');
 exports.deleteTrip = async (tripId) => await Trip.findByIdAndDelete({ _id: tripId });
-exports.editTrip = async (tripId, tripData) => await Trip.findByIdAndUpdate({ _id: tripId }, tripData );
+exports.editTrip = async (tripId, tripData) => await Trip.findByIdAndUpdate({ _id: tripId }, tripData);
+exports.getAuthor = async (authorId) => await User.findById(authorId);
+exports.addOffer = async (offer, authorId) => await User.findByIdAndUpdate({ _id: authorId }, { $push: { tripsHistory: offer } });
+exports.myOffers = async (userId) => await Trip.find().where({ owner: userId });
+exports.joinTrip = async (tripId, userId) => await Trip.findByIdAndUpdate({ _id: tripId }, { $push: { buddies: userId }});
+exports.editSeats = async (tripId, seats) => await Trip.findByIdAndUpdate({ _id: tripId }, { seats: seats });
