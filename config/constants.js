71ef1b31f65d872e97068021ae03d6ad7d2c4426
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-10-11 23:14:27 +0300

    deploy app in azure using docker

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,3 @@
-exports.CONNECTION_STRING = "mongodb://127.0.0.1:27017/Trips";
+exports.CONNECTION_STRING = "mongodb+srv://martin13s18:01234567890Mero.@atlascluster.vbqdt4k.mongodb.net/SharedTrips";
 exports.SECRET = "qwer546yutiop0jnmmjhy7788adstr66766ujghjg78u8t78u";
 exports.SALT_ROUNDS = 10;
